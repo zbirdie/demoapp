ff41c41fdbb813f48f75eb9f4dafbe2b08f8c060
Zarrah <zarrahb@berkeley.edu>
2016-09-23 00:44:28 +0000

    added some tests for static_pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,14 @@
-class StaticPagesController < ApplicationController
+class StaticPagesController < ApplicationController:: TestCase
+  test "should get home" do
+    get :home
+    assert_response :successfully
+    
+  end
   def home
   end
 
   def help
   end
 end
+
+
